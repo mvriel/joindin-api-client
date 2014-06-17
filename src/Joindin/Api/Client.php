4fb422f84e45222a00446db2caabf0161d6a549c
Mike van Riel <me@mikevanriel.com>
2014-06-17 21:00:12 +0200

    Create a Client and add Guzzle
    As a start for creating an API Client for JoindIn we need
    a client that can (optionally) handle authentication using
    an access token and that Guzzle is loaded.

---
+++ src/Joindin/Api/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Joindin\Api;
+
+use GuzzleHttp\Collection;
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client extends GuzzleClient
+{
+    const DEFAULT_BASE_URL = 'http://api.joind.in/v2.1';
+
+    public function __construct($config = array())
+    {
+        $defaults = array('base_url' => self::DEFAULT_BASE_URL);
+        $required = array('base_url');
+
+        $configuration = Collection::fromConfig($config, $defaults, $required);
+
+        parent::__construct($configuration->toArray());
+
+        if ($configuration->get('access_token')) {
+            $this->setDefaultOption('headers/Authorization', 'OAuth ' . $configuration->get('access_token'));
+        }
+        $this->setDefaultOption('headers/Accept-Charset', 'utf-8');
+    }
+}
