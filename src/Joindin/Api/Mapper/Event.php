3be26d63e1272957ef8f476b2d548e03c62523ad
Mike van Riel <me@mikevanriel.com>
2014-06-18 08:30:32 +0200

    Add Event Entity and mapping
    In this commit we add an Event entity that will be used to catch the
    contents of event-related responses. Associated with it is a mapper
    that can convert an associative array representing the Event entity
    into an Event object.

---
+++ src/Joindin/Api/Mapper/Event.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Joindin\Api\Mapper;
+
+
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+class Event implements MapperInterface
+{
+    private $camelizedAttributes = array(
+        'url_friendly_name',
+        'start_date',
+        'end_date',
+        'attendee_count',
+        'event_comments_count',
+        'tracks_count',
+        'talks_count',
+        'verbose_uri',
+        'comments_uri',
+        'talks_uri',
+        'tracks_uri',
+        'attending_uri',
+        'website_uri',
+        'humane_website_uri',
+        'attendees_uri'
+    );
+
+    /**
+     * @var GetSetMethodNormalizer
+     */
+    private $normalizer;
+
+    public function __construct(GetSetMethodNormalizer $normalizer)
+    {
+        $this->normalizer = clone $normalizer;
+        $this->normalizer->setCamelizedAttributes($this->camelizedAttributes);
+    }
+
+    public function map(array $data)
+    {
+        return $this->normalizer->denormalize($data, 'Joindin\Api\Entity\Event');
+    }
+} 
