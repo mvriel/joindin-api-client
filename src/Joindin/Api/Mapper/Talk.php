c3b03b2d5c49a59fd0929dcb6e30d5c4297ec7aa
Mike van Riel <me@mikevanriel.com>
2014-06-18 20:11:58 +0200

    Add support for Talks
    In this commit I have added the option to retrieve a series of talks for an
    event's talks_url, one specific talk by its own URL and to submit a new talk
    on the talks collection for an event (identified by the talks_url).

---
+++ src/Joindin/Api/Mapper/Talk.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Joindin\Api\Mapper;
+
+
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+class Talk implements MapperInterface
+{
+    private $camelizedAttributes = array(
+        'talk_title',
+        'url_friendly_talk_title',
+        'talk_description',
+        'slides_link',
+        'start_date',
+        'average_rating',
+        'comments_enabled',
+        'comment_count',
+        'verbose_uri',
+        'website_uri',
+        'comments_uri',
+        'verbose_comments_uri',
+        'event_uri',
+        'starred_uri',
+    );
+
+    /**
+     * @var GetSetMethodNormalizer
+     */
+    private $normalizer;
+
+    public function __construct(GetSetMethodNormalizer $normalizer)
+    {
+        $this->normalizer = clone $normalizer;
+        $this->normalizer->setCamelizedAttributes($this->camelizedAttributes);
+    }
+
+    public function map(array $data)
+    {
+        return $this->normalizer->denormalize($data, 'Joindin\Api\Entity\Talk');
+    }
+} 
